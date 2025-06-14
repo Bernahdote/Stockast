e85c2bb09b544c181d2b2fb9524b681a98bbcd42
Willem Berner <willemberner@Carl-Willems-MacBook-Air.local>
2025-06-15 01:54:17 +0200

    Updated api.py with functionality of sectors, markets and text read in podcast tone

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
 from pydantic import BaseModel
+import ast
 import uvicorn
 from tts import speak_and_download, TTSRequest
-from scrape import understand_input, get_keys, get_news
+from scrape import understand_tickrs, get_keys, get_news, understand_markets, understand_sectors, get_technical_summary, get_sector_news, get_market_news, generate_podcast
 
 app = FastAPI()
 
@@ -12,23 +13,40 @@ class TextRequest(BaseModel):
     voice_id: str = "21m00Tcm4TlvDq8ikWAM"  # Default voice ID for ElevenLabs
 
 # RUN LOCAL: python api.py
-# EXAMPLE: curl -X POST -H "Content-Type: application/json" http://localhost:8000/process-text --data '{"text": "interested in Google, Nividia and Apple stocks"}'
+# EXAMPLE: curl -X POST -H "Content-Type: application/json" http://localhost:8000/process-text --data '{"text": "I'm interested in Apple and health services and crypto" }'
+# EXAMPLE: curl -X POST http://localhost:8000/process-text -H "Content-Type: application/json" --data "{\"text\":\"I'm interested in Apple and health services and crypto\"}" --output podcast.mp3 --write-out "\nHTTP status: %{http_code} — bytes: %{size_download}\n"
 @app.post("/process-text")
 async def process_text(request: TextRequest):
     if not request.text:
         raise HTTPException(status_code=400, detail="Text cannot be empty")
 
     try:
-        keys = understand_input(request.text)
+        input = request.text
+        keys = understand_tickrs(input) 
+        sectors = understand_sectors(input) 
+        markets = understand_markets(input)
 
-        sum = ""
-        for key in keys: 
-            keyfacts = get_keys(key) 
-            news = get_news(key)
-            sum = sum.join([keyfacts, news])
+        summary = ""
 
+        for key in keys:
+            summary += get_keys(key) 
+            summary += get_technical_summary(key)
+            summary += get_news(key) 
 
-        audio_data = speak_and_download(TTSRequest(text=sum, voice_id=request.voice_id))
+        sectors  = ast.literal_eval(sectors)   
+        markets  = ast.literal_eval(markets)  
+
+        for sec in sectors:
+            summary += get_sector_news(sec) 
+
+        for market in markets:
+            summary += get_market_news(market)
+        
+        podcast = generate_podcast(summary)
+        
+        tts_req   = TTSRequest(text=podcast, voice_id=request.voice_id)
+        audio_data = speak_and_download(**tts_req.model_dump())     
+
         if isinstance(audio_data, dict) and "error" in audio_data:
             raise HTTPException(status_code=500, detail=audio_data["error"])
         return Response(content=audio_data, media_type="audio/mpeg")
